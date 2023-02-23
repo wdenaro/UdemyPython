6d2582e31cc8a90387d0aa0f1a52f95f1216a523
Wesley Denaro <Wesley.Denaro@iCloud.com>
2023-02-23 08:53:57 -0500

    I still do not like unit testing

--- 15.197_unit_testing/main.py
+++
@@ -1,6 +0,0 @@
-def do_stuff(num):
-    try:
-        return int(num) + 5
-
-    except ValueError as err:
-        return err
--- 15.197_unit_testing/test.py
+++ 15.197_unit_testing/test.py
@@ -1,18 +1,44 @@
 import unittest
-import main
+import script
 
 
 class TestMain(unittest.TestCase):
 
-    def test_do_stuff(self):
+    def test_with_integer(self):
         test_param = 10
-        result = main.do_stuff(test_param)
+        result = script.do_stuff(test_param)
         self.assertEqual(result, 15)
 
-    def test_do_stuff2(self):
+    def test_with_zero(self):
+        test_param = 0
+        result = script.do_stuff(test_param)
+        self.assertEqual(result, 5)
+
+    def test_with_string(self):
         test_param = 'qwerty'
-        result = main.do_stuff(test_param)
+        result = script.do_stuff(test_param)
         self.assertTrue(isinstance(result, ValueError))
 
+    def test_with_empty_string(self):
+        test_param = ''
+        result = script.do_stuff(test_param)
+        self.assertEqual(result, 'parameter must be a number')
 
-unittest.main()
+    def test_with_none(self):
+        test_param = None
+        result = script.do_stuff(test_param)
+        self.assertEqual(result, 'parameter must be a number')
+
+    def test_with_float(self):
+        test_param = 3.14159
+        result = script.do_stuff(test_param)
+        self.assertFalse(isinstance(result, ValueError))
+
+    def test_with_list(self):
+        test_param = ['w', 'd']
+        result = script.do_stuff(test_param)
+        self.assertTrue(isinstance(result, TypeError))
+
+
+if __name__ == '__main__':
+    unittest.main()
